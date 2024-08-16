dd95f1eac071755580ff096e3b3b3266ff057a9b
Misha Che <misha2004546@gmail.com>
2024-08-16 21:13:47 +0300

    hw 6: created configs for gunicorn & nginx

---
+++ gunicorn.conf.py
@@ -0,0 +1,9 @@
+import multiprocessing
+
+bind = '127.0.0.1:8081'
+workers = multiprocessing.cpu_count() * 2 + 1
+
+accesslog = '/var/tmp/gunicorn_access.log'
+#errorlog  = 'var/tmp/gunicorn_error.log'
+
+
